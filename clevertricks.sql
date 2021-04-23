557908bb59891d6b2eba1f61413d7efa37fd2fd8
arnaud-blanc <mail.github2@blaca.eu>
2021-04-23 11:28:56 +0200

    Update README.md, clevertricks.sql and add myTwelveFirstTricks.sql

--- clevertricks.sql
+++ clevertricks.sql
@@ -1,66 +1,58 @@
--- 'tabula rasa' in database
+DROP TABLE IF EXISTS `tricks_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 
-DROP TABLE IF EXISTS tricks_category;
-DROP TABLE IF EXISTS tricks;
-DROP TABLE IF EXISTS category;
+--
+-- Table structure for table `category`
+--
 
--- creation of tables
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE tricks (
-id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(100) NOT NULL,
-content VARCHAR(2000) NOT NULL
-);
+--
+-- Table structure for table `item`
+--
 
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-CREATE TABLE category (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(100) NOT NULL
-);
+--
+-- Table structure for table `tricks`
+--
 
--- join table of the two previous tables
+DROP TABLE IF EXISTS `tricks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tricks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(2000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE tricks_category (
-id INT PRIMARY KEY AUTO_INCREMENT,
-tricks_id INT,
-category_id INT,
-FOREIGN KEY (tricks_id) REFERENCES tricks(id),
-FOREIGN KEY (category_id) REFERENCES category(id)
-);
+--
+-- Table structure for table `tricks_category`
+--
 
-
--- insert data in the corresponding tables
-
-INSERT INTO category (name) VALUES ('ide'), ('os'), ('terminal'), ('git/github');
-
-INSERT INTO tricks (title, content) 
-VALUES 
-('utiliser screenfetch pour s\'informer sur notre OS', 
-'Screenfetch est un petit script bash, qui permet d’afficher dans un terminal une série d’informations concernant votre distribution GNU/Linux. 
-Installation : $ sudo apt update && sudo apt install screenfetch
-Voir le manpage en lien'),
-('créer un petit script pour mettre à jour notre système',
-'Dans un terminal créer un fichier maj (pour mise à jour):
-$ touch maj 
-puis l ouvrir avec un éditeur de texte et insérer ce qui suit: 
-$ gedit maj 
-#!/bin/bash 
-sudo apt update && sudo apt upgrade 
-enregistrer et lui donner les droits d exécution : 
-$ chmod +x maj 
-l exécuter : 
-$ ./maj'),
-('agrandir / réduire la fenetre dans vscode','touche windows + flèche haut / touche windows + flèche bas'),
-('fermer le document actif dans vscode','CTRL + F4'),
-('afficher la liste des fichiers ouverts dans vscode','CTRL + ALT + flèche bas'),
-('afficher toutes les fenêtres flottantes dans vscode','CTRL + MAJ + M'),
-('démarrer une nouvelle instance dans vscode','touche windows + MAJ + N'),
-('basculer entre les fenêtres dans vscode','touche windows + N'),
-('créer une branche', 'git branch <BRANCHNAME>'),
-('Basculer sur autre branche', 'git checkout <BRANCHNAME>'),
-('Vérifier le contenu','git status'),
-('faire un clone', 'git clone <URLFROMGITHUB>');
-
-
-INSERT INTO tricks_category (tricks_id, category_id) VALUES
-(1, 3), (2, 3), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1), (9, 4), (10, 4), (11, 4), (12, 4);
+CREATE TABLE `tricks_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tricks_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tricks_id` (`tricks_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `tricks_category_ibfk_1` FOREIGN KEY (`tricks_id`) REFERENCES `tricks` (`id`),
+  CONSTRAINT `tricks_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
