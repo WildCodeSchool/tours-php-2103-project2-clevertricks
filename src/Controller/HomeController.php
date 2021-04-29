902bf5dee9bb115b087ccce0665720b392336f2c
arnaud-blanc <mail.github2@blaca.eu>
2021-04-30 01:47:29 +0200

    Update HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,7 @@ namespace App\Controller;
 use App\Model\CategoryManager;
 use App\Model\TricksManager;
 
-class HomeController extends AbstractController
+class HomeController extends MyAbstractController
 {
     /**
      * Display home page
@@ -26,13 +26,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $categoryManager = new CategoryManager();
-        $categories = $categoryManager->selectAll('name');
         $tricksManager = new TricksManager();
         $lastFiveTricks = $tricksManager->selectJoin('tricks_id', 'DESC', '5');
         return $this->twig->render(
             'Home/index.html.twig',
-            ['categories' => $categories, 'lastFiveTricks' => $lastFiveTricks]
+            ['lastFiveTricks' => $lastFiveTricks]
         );
     }
 }
